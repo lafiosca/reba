4ce291706e50c88311384c7f57736db33fbb4c52
Joe Lafiosca <joe@lafiosca.com>
2022-04-27 16:37:06 -0400

    update example config

--- src/config.example.ts
+++ src/config.example.ts
@@ -1,21 +1,34 @@
-const config = {
+import { Config } from './types';
+
+const config: Config = {
 	postmaster: 'you@yourteam.awsapps.com',
-	aliases: [
+	globalRules: {
+		rejectIfSubjectContains: ['MAKE.MONEY.FAST'],
+	},
+	rules: [
 		{
-			pattern: /@yourdomain\.com$/i,
-			rejectPattern: /^(spamaddress|otherspam)@/i,
+			matchHost: 'yourdomain.com',
+			rejectUsers: [
+				'bad',
+				'null',
+			],
 			recipients: ['you@yourteam.awsapps.com'],
 		},
 		{
-			pattern: /^buddy@yourotherdomain\.org$/i,
+			matchExact: 'buddy@yourotherdomain.org',
 			recipients: ['buddy@yourteam.awsapps.com'],
 		},
 		{
-			pattern: /^wholeteam@yourotherdomain\.org$/i,
+			matchPattern: /^(biz|legal)@yourotherdomain\.org$/i,
 			recipients: [
 				'you@yourteam.awsapps.com',
 				'buddy@yourteam.awsapps.com',
 			],
+		},
+		{
+			matchPattern: /^(abuse|admin|(web|post|host)master)@/i,
+			allowAll: true,
+			recipients: ['you@yourteam.awsapps.com'],
 		},
 	],
 };
